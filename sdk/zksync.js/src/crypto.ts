135d522edaea19a4f58366b586bdeb483d5eb9cf
Nicholas Vincent <nick@quantik.solutions>
2020-10-20 00:14:21 -0400

    Beta Implementation of native module

--- sdk/zksync.js/src/crypto.ts
+++ sdk/zksync.js/src/crypto.ts
@@ -1,6 +1,6 @@
 import { Signature } from "./types";
 // @ts-ignore
-import ZkSync from 'react-native-zksync';
+import ZkSync from '@quantik-solutions/react-native-zksync';
 import { utils } from "ethers";
 
 export async function privateKeyFromSeed(privateKey: Uint8Array): Promise<Uint8Array> {
@@ -10,8 +10,8 @@ export async function privateKeyFromSeed(privateKey: Uint8Array): Promise<Uint8A
 // TODO: Check this, the substr in between what seems to be concatenated strings
 export async function signTransactionBytes(privKey: Uint8Array, bytes: Uint8Array): Promise<Signature> {
     const signaturePacked = await ZkSync.signMusig(utils.toUtf8String(privKey), utils.toUtf8String(bytes));
-    const pubKey = utils.hexlify(signaturePacked.slice(0, 32)).substr(2);
-    const signature = utils.hexlify(signaturePacked.slice(32)).substr(2);
+    const pubKey = signaturePacked.slice(0, 30);
+    const signature = signaturePacked.slice(30);
     return {
         pubKey,
         signature
