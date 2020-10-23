c902cdff2e9e9e3aaae6328c57b10fde2d290cba
Nicholas Vincent <nick@quantik.solutions>
2020-10-23 00:06:26 -0400

    version 0.0.20 includes string interpolation fix

--- sdk/zksync.js/src/crypto.ts
+++ sdk/zksync.js/src/crypto.ts
@@ -4,14 +4,13 @@ import ZkSync from '@quantik-solutions/react-native-zksync';
 import { utils } from "ethers";
 
 export async function privateKeyFromSeed(privateKey: Uint8Array): Promise<Uint8Array> {
-    return await ZkSync.privateKeyFromSeed(utils.toUtf8String(privateKey))
+    return utils.arrayify(`0x${await ZkSync.privateKeyFromSeed(utils.hexlify(privateKey).substr(2))}`)
 }
 
-// TODO: Check this, the substr in between what seems to be concatenated strings
 export async function signTransactionBytes(privKey: Uint8Array, bytes: Uint8Array): Promise<Signature> {
     const signaturePacked = await ZkSync.signMusig(utils.hexlify(privKey).substr(2), utils.hexlify(bytes).substr(2));
-    const pubKey = utils.hexlify(utils.arrayify(`$0x{signaturePacked}`).slice(0, 32)).substr(2)
-    const signature = utils.hexlify(utils.arrayify(`$0x{signaturePacked}`).slice(32)).substr(2)
+    const pubKey = utils.hexlify(utils.arrayify(`0x${signaturePacked}`).slice(0, 32)).substr(2)
+    const signature = utils.hexlify(utils.arrayify(`0x${signaturePacked}`).slice(32)).substr(2)
     return {
         pubKey,
         signature
@@ -19,5 +18,5 @@ export async function signTransactionBytes(privKey: Uint8Array, bytes: Uint8Arra
 }
 
 export async function privateKeyToPubKeyHash(privateKey: Uint8Array): Promise<string> {
-    return await ZkSync.publicKeyHashFromPrivateKey(utils.toUtf8String(privateKey));
+    return await ZkSync.publicKeyHashFromPrivateKey(utils.hexlify(privateKey).substr(2));
 }
