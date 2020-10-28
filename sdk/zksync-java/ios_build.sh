dfc6a4e9ea3cb419c08b2c4299f7d973041dfc1d
Nicholas Vincent <nick@quantik.solutions>
2020-10-28 02:28:42 -0400

    iOS module
    testing still needs to be done

--- sdk/zksync-java/ios_build.sh
+++ sdk/zksync-java/ios_build.sh
@@ -4,11 +4,11 @@ cbindgen src/lib.rs -l c > ZkSync.h
 cargo lipo --release
 
 # we're still in the `rust` folder so...
-inc=../ios/include
-libs=../ios/libs
+inc=../../../ios/include
+libs=../../../ios/libs
 
 mkdir ${inc}
 mkdir ${libs}
 
 cp ZkSync.h ${inc}
-cp target/universal/release/librustylib.a ${libs}
+cp target/universal/release/libzksync.a ${libs}
