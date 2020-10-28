0ae7b9dabf8ebc33119887a37f7103d5b8dd5f20
Nicholas Vincent <nick@quantik.solutions>
2020-10-27 21:05:08 -0400

    ios build target removed

--- rust_build.sh
+++ rust_build.sh
@@ -14,7 +14,7 @@ source $HOME/.cargo/env
 # Android targets
 rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
 # iOS targets
-rustup target add aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
+rustup target add aarch64-apple-ios armv7s-apple-ios x86_64-apple-ios i386-apple-ios
 
 cargo install cargo-ndk
 cargo install cargo-lipo
